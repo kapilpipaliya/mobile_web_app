bb9af7850401119550ed065a5022af565852acd4
abhi <vaghaniabhay3532@gmail.com>
2023-08-25 09:12:00 +0530

    update progress notification and deep link.

--- lib/provider/download_provider.dart
+++ lib/provider/download_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -8,44 +10,55 @@ import 'package:path/path.dart' as path;
 class DownloadProvider extends ChangeNotifier {
   download(String url) async {
     NotificationService notificationService = NotificationService();
+    Timer? notificationTimer;
     try {
       String filePath = '';
       final dio = Dio();
+      int progress = 0;
+      notificationTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+        notificationService.createNotification(100, progress, filePath);
+        if (progress == 100) {
+          Fluttertoast.showToast(msg: "File downloaded successfully");
+          timer.cancel();
+        }
+      });
       String basename = path.basename(url);
       filePath = path.join("/storage/emulated/0/Download", basename);
       await dio.download(url, filePath,
           onReceiveProgress: ((count, total) async {
-        await Future.delayed(const Duration(seconds: 1), () {
-          notificationService.createNotification(
-              100, ((count / total) * 100).toInt(), filePath);
-          notifyListeners();
-        });
+        progress = ((count / total) * 100).toInt();
+        // await Future.delayed(const Duration(seconds: 1), () {
+        //   notifyListeners();
+        // });
       }));
-      notificationService.createNotification(100, 100, filePath);
-      Fluttertoast.showToast(msg: "File downloaded successfully");
     } on DioException catch (e) {
       print("error downloading file $e");
+      if (notificationTimer != null) {
+        notificationTimer.cancel();
+        notificationService.createNotification(0, 0, '', error: e.message);
+      }
     }
   }
 }
 
 class NotificationService {
-  void createNotification(int count, int i, String filePath) {
+  void createNotification(int count, int i, String filePath, {String? error}) {
     var androidPlatformChannelSpecifics = (count != i)
         ? AndroidNotificationDetails('progress channel', 'progress channel',
             channelDescription: 'progress channel description',
             channelShowBadge: false,
-            importance: Importance.max,
-            priority: Priority.high,
+            importance: Importance.defaultImportance,
             onlyAlertOnce: true,
             showProgress: true,
+            playSound: false,
+            enableVibration: false,
             maxProgress: count,
             progress: i)
         : const AndroidNotificationDetails(
-            'progress channel',
-            'progress channel',
-            channelDescription: 'progress channel description',
-            channelShowBadge: false,
+            'success channel',
+            'success channel',
+            channelDescription: 'success channel description',
+            channelShowBadge: true,
             importance: Importance.max,
             priority: Priority.high,
           );
@@ -56,9 +69,15 @@ class NotificationService {
           0, 'Downloading file...', '$i%', platformChannelSpecifics,
           payload: 'item x');
     } else {
-      flutterLocalNotificationsPlugin.show(0, 'File downloaded successfully',
-          'Click to view file', platformChannelSpecifics,
-          payload: 'file:$filePath');
+      if (error != null) {
+        flutterLocalNotificationsPlugin.show(
+            0, 'Download failed', error, platformChannelSpecifics,
+            payload: 'file:$filePath');
+      } else {
+        flutterLocalNotificationsPlugin.show(0, 'File downloaded successfully',
+            'Click to view file', platformChannelSpecifics,
+            payload: 'file:$filePath');
+      }
     }
   }
 }
